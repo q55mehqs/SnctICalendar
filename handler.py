79051eb00dc18dc16a86206e14509533bae10d64
q55mehqs <s1701057@sendai-nct.jp>
2021-04-30 11:14:42 +0900

    fix some errors

--- handler.py
+++ handler.py
@@ -13,41 +13,51 @@ grade_columns = {
     7: '専2'
 } # type: Dict[int, Union[None, str]]
 
+def make_grade_text(grade: int) -> str:
+    if grade > 0 and grade < 6:
+        return f"{grade}年"
+    elif grade == 6:
+        return "専1"
+    elif grade == 7:
+        return "専2"
+    else:
+        return "全学年"
+
 class CalendarManager:
     def __init__(self, grade: int):
         self.grade = grade
-        grade_text = f"{grade}年"
-        if grade == 6:
-            grade_text = "専1"
-        elif grade == 7:
-            grade_text = "専2"
-        else:
-            grade_text = "全学年"
-
-        self.grade_text = grade_text
+        self.grade_text = make_grade_text(grade)
 
         self.text = (
-            "BEGIN:VCALENDAR\n" +
-            "VERSION:2.0\n" +
-            "PRODID:-//jugyobot//q55mehqs//KadaiCalendar//JP\n" +
-            f"X-WR-CALNAME:仙台高専 年間行事予定 ({grade_text})" +
-            "X-WR-TIMEZONE:JST" +
-            f"X-WR-CALDESC:仙台高専 {grade_text}の年間行事予定")
+            "BEGIN:VCALENDAR\r\n" +
+            "PRODID:-//jugyobot//q55mehqs//KadaiCalendar//JP\r\n" +
+            "VERSION:2.0\r\n" +
+            "CALSCALE:GREGORIAN\r\n" +
+            "METHOD:PUBLISH\r\n" +
+            f"X-WR-CALNAME:仙台高専 年間行事予定 ({self.grade_text})\r\n" +
+            "X-WR-TIMEZONE:Asia/Tokyo\r\n" +
+            f"X-WR-CALDESC:仙台高専 {self.grade_text}の年間行事予定\r\n")
 
     def add_data(self, schedule):
+        if self.grade == 0 and not schedule['全学年予定']:
+            return
+
         f = '%Y%m%d'
         stdate = datetime.strptime(schedule['開始日'], "%Y-%m-%d")
-        endate = datetime.strptime(schedule['開始日'], "%Y-%m-%d") if not schedule['終了日'] else stdate + timedelta(days=1)
+        endate = datetime.strptime(schedule['終了日'], "%Y-%m-%d") if schedule['終了日'] else stdate
+        endate = endate + timedelta(days=1)
 
         summary = self.make_summary(schedule)
+        if not summary:
+            return
 
         self.text += (
-            "BEGIN:VEVENT\n" +
-                f"DTSTART;TZID=Asia/Tokyo;VALUE=DATE:{datetime.strftime(stdate, f)}\n" +
-                f"DTEND;TZID=Asia/Tokyo;VALUE=DATE:{datetime.strftime(endate, f)}\n" +
-                f"SUMMARY:{summary}\n" +
-                f"DESCRIPTION:{summary}\n" +
-            "END:VEVENT\n"
+            "BEGIN:VEVENT\r\n" +
+                f"DTSTART;VALUE=DATE:{datetime.strftime(stdate, f)}\r\n" +
+                f"DTEND;VALUE=DATE:{datetime.strftime(endate, f)}\r\n" +
+                f"SUMMARY:{summary}\r\n" +
+                f"DESCRIPTION:{summary}\r\n" +
+            "END:VEVENT\r\n"
         )
 
     def make_summary(self, schedule: Dict[str, str]) -> str:
@@ -57,10 +67,17 @@ class CalendarManager:
 
         column = grade_columns[self.grade]
         if column is None:
-            return summary_data[0]
+            for i in range(1,8):
+                col = grade_columns[i]
+                if col == None:
+                    continue
+
+                if schedule[col]:
+                    summary_data.append(f'[{make_grade_text(i)}] {schedule[col]}')
+            return ', '.join(summary_data)
 
         if schedule[column]:
-            summary_data.append(f'({self.grade_text}) {schedule[column]}')
+            summary_data.append(f'[{self.grade_text}] {schedule[column]}')
 
         return ', '.join(summary_data)
 
@@ -71,11 +88,20 @@ def now_year():
     today = date.today()
     return today.year if today.month <= 4 else today.year - 1
 
-def handler(event: Dict[str, Any], context):
-    grade = event['grade'] if 'grade' in event else 0 # type: int
+def lambda_handler(event: Dict[str, Any], context):
+    print(event)
+    if event['queryStringParameters']:
+        param = event['queryStringParameters']
+        year = int(param['year']) if 'year' in param and param['year'].isdigit() else now_year()
+
+        grade = int(param['grade']) if 'grade' in param and param['grade'].isdigit() else 0
+    else:
+        year = now_year()
+        grade = 0
+
     manager = CalendarManager(grade)
 
-    with open(f'SnctSchedules/{now_year()}.csv', 'r', encoding='utf-8', newline="") as f:
+    with open(f'SnctSchedules/{year}.csv', 'r', encoding='utf-8', newline="") as f:
         ds = csv.DictReader(f)
 
         for d in ds:
